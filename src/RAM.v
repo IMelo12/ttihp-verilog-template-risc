eaac90ce89a5bf736166cec44ff06ed5777ba338
Isaac Melo <isaac.melo426@gmail.com>
2025-05-15 01:11:50 -0400

    Update RAM.v

--- src/RAM.v
+++ src/RAM.v
@@ -3,16 +3,16 @@ module RAM #(parameter DEPTH=10, parameter WIDTH=8)(
     input write_enable,
     input [WIDTH-1:0] data,
     input [DEPTH-1:0] address,
-    output reg [WIDTH-1:0] data_out
+    output reg [31:0] data_out
 );
 
 
-    reg[WIDTH-1:0] ram_module[0:2**DEPTH - 1];
+    reg[31:0] ram_module[0:2**DEPTH - 1];
 integer i;
 
 initial begin
     for(i = 0; i < DEPTH; i = i + 1) 
-        ram_module[i] <= {WIDTH{1'b0}};
+        ram_module[i] <= {32{1'b0}};
 end
 
 always @(posedge clk) begin
